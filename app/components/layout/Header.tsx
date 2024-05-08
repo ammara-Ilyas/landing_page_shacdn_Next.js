3ede836e54f76091fde6dc10d1556fffca332548
ammara-Ilyas <ismarailyas545@gmail.com>
2024-05-08 17:22:00 +0500

    Header added

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/images/assets/portfolio/Logo.png";
+import { Button } from "@/components/ui/button";
+import { VscThreeBars } from "react-icons/vsc";
+import { RxCross2 } from "react-icons/rx";
+function Header() {
+  const [isOpened, setIsOpenedl] = useState(false);
+
+  const navLink = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "about",
+      path: "#about",
+    },
+    {
+      name: "Services",
+      path: "#services",
+    },
+    {
+      name: "Project",
+      path: "#project",
+    },
+
+    {
+      name: "Contact",
+      path: "#contact",
+    },
+  ];
+  return (
+    <header className="bg-rose-200 text-black  font-bold grid grid-cols-9 items-center py-3 duration-75 sticky top-0 z-40">
+      <div className="rounded-full hidden bg-red-500 absolute left-1/3 w-28 h-28 -top-3/4 z-100"></div>
+      <div className="w-40 col-start-1 pl-10">
+        <Image src={logo} alt="logo" className="w-full" />
+      </div>
+      <nav
+        className={`${
+          !isOpened ? "hidden" : "block"
+        }  navLink col-start-4 col-end-10 pt-2 row-start-0 md:block  bg-red-400 bg-opacity-90 md:bg-transparent md:row-start-1  md:w-1/2  w-1/2  md:col-start-4 md:gap-5  md:row-end-1 md:relative  absolute top-0 right-0 md:h-auto h-screen  `}
+      >
+        {" "}
+        <div className="pl-5 pt-5 text-3xl md:hidden">
+          {" "}
+          <RxCross2 onClick={() => setIsOpenedl(false)} />{" "}
+        </div>
+        <ul className="flex   md:justify-between flex-col text-2xl md:text-lg w-full mt-24 md:m-0 gap-5 items-center h-full md:flex-row md:gap-2">
+          {navLink.map((item, i) => (
+            <li
+              className=" text-left w-1/2 md:w-auto capitalize"
+              key={i}
+              onClick={() => setIsOpenedl(!isOpened)}
+            >
+              <Link href={item.path}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className="col-start-9 row-start-1 md:col-start-8 md:col-end-10 flex justify-center">
+        <div className="hidden md:block ">
+          <Button variant="hire" size="lg">
+            Portfolio
+          </Button>
+        </div>
+        <div className="block text-2xl md:hidden col-start-10 ml-8">
+          <VscThreeBars onClick={() => setIsOpenedl(!isOpened)} />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
